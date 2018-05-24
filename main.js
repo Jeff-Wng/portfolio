470b601491dc2e5c62c9e747ceed7e7537ef38ff
jwong421 <jwong1421@gmail.com>
2018-05-23 20:39:17 -0400

    responsive

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
     $('.threeBars').click(function() {
         $('.Nav').toggle("slide", {direction: 'right'}, 200);
+        $('.Nav').toggleClass('displayNone');
         $(".Bar1").toggleClass('change1');
         $(".Bar2").toggleClass('change2');
         $(".Bar3").toggleClass('change3');
